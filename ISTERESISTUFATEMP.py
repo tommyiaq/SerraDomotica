750320a25d63edd655d137e4be117f41a3ba8fe3
Tommaso Iaquinta <tommyiaq22@gmail.com>
2017-01-08 19:22:40 +0000

    Aggiunto spegnimento di sicurezza in caso il sensore non funzionasse

--- ISTERESISTUFATEMP.py
+++ ISTERESISTUFATEMP.py
@@ -12,19 +12,26 @@ umi, t = Adafruit_DHT.read_retry(sensor, pinsensor)
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pinrele, GPIO.OUT)
+min = 22
+max = 23
 
 try:
 #	umi, t = Adafruit_DHT.read_retry(sensor, pinsensor)
 
-	if t is not None and t < 22:
-		
+	if t < min:
 		GPIO.output(pinrele, GPIO.LOW)
 		print('Temp={0:0.1f}*C Umidita={1:0.1f}%'.format(t, umi))
-	elif t is not None and t > 23:
+	elif t > max:
                 GPIO.output(pinrele, GPIO.HIGH)
                 print('Temp={0:0.1f}*C Umidita={1:0.1f}%'.format(t, umi))
-	elif t is not None and t >= 22 and t <= 23:
+	elif t >= min and t <= max:
                 print('Temp={0:0.1f}*C Umidita={1:0.1f}%'.format(t, umi))
+	elif t is None:
+	   	GPIO.output(pinrele, GPIO.HIGH)
+           	print('Il sensore la ha scialata')
+	elif t < 0:
+		GPIO.output(pinrele, GPIO.HIGH)
+		print('Il sensore ha dei problemi esce dal range 0-50C*')
 	else:
      		print('Il sensore la ha scialata')
 
