889c4408b42bd45bfef9150be265e0a2bda1783a
Tommaso Iaquinta <tommyiaq22@gmail.com>
2017-01-28 11:31:04 +0100

    I due script nel caso di uscita None del sensore continuavano a richiedere la temp all'infinito questo portava a un accumolo di processi avviati che a loro volta peggioravano la possibilita di avere una lettura corretta. Adesso dopo 4 tentativa esce anche senza dato.

--- NOTTEGIORNOSTUFA.py
+++ NOTTEGIORNOSTUFA.py
@@ -24,22 +24,22 @@ luce = not(localtime()[3] in (f1 + f2))	#
 venti = GPIO.input(pinventi)
 min = 22
 max = 22.5
-deltavg = (1.0, 1.2)
+deltavg = (1.3, 1.4)
 
-minN = 19.5
-maxN = 19.8
-deltav = (1.2, 1.3)
+minN = 19.4
+maxN = 19.7
+deltav = (1.1, 1.2)
 
 i = 0
 
-while i < 1:
+while i < 4:
 	if luce == 0 and luce is not None:	 
 		if t < min and t is not None:
 			GPIO.output(pinrele, GPIO.LOW)
 			GPIO.output(pinventi, GPIO.HIGH)
 			print('Temp={0:0.1f}*C Umidita={1:0.1f}%'.format(t, umi))
                         print('Temperatura diurna bassa, stufa ON, luci='+repr(luce)+', ventilatore='+repr(venti))
-			i = 1
+			i = 4
 		elif t > max and t is not None:
 	                GPIO.output(pinrele, GPIO.HIGH)
                         if t > max + deltavg[1]:
@@ -48,32 +48,37 @@ while i < 1:
 	                        GPIO.output(pinventi, GPIO.HIGH)
 			print('Temp={0:0.1f}*C Umidita={1:0.1f}%'.format(t, umi))
                         print('Temperatura diurna alta, stufa OFF, luci='+repr(luce)+', ventilatore='+repr(venti))
-			i = 1
+			i = 4
 		elif t >= min and t <= max and t is not None:
                         GPIO.output(pinventi, GPIO.HIGH)
         	        print ('Temp={0:0.1f}*C Umidita={1:0.1f}%'.format(t, umi))
                         print('Temperatura diurna OK, stufa OFF, luci='+repr(luce)+', ventilatore='+repr(venti))
-			i = 1	
+			i = 4	
 		elif t is None:
                         GPIO.output(pinventi, GPIO.HIGH)
 		   	GPIO.output(pinrele, GPIO.HIGH)
            		print('Il sensore la ha scialata, None')
 	                umi, t = Adafruit_DHT.read_retry(sensor, pinsensor, 5, 1)
+                        i = i + 1
+                        print i
 		elif t < 0 and t > 50:
                         GPIO.output(pinventi, GPIO.HIGH)
 			GPIO.output(pinrele, GPIO.HIGH)
 			print('Il sensore ha dei problemi esce dal range 0-50C*')
         	        umi, t = Adafruit_DHT.read_retry(sensor, pinsensor)
+			i = i + 1
+			print i
 		else:
      			print('Il sensore la ha scialata')
                		umi, t = Adafruit_DHT.read_retry(sensor, pinsensor, 5, 1)
+			i = i + 1
 	elif luce == 1 and luce is not None:
                 if t < minN and t is not None:
                         GPIO.output(pinrele, GPIO.LOW)
                         GPIO.output(pinventi, GPIO.HIGH)
                         print('Temp={0:0.1f}*C Umidita={1:0.1f}%'.format(t, umi))
                         print('Temperatura notturna bassa, stufa ON, luci='+repr(luce)+', ventilatore='+repr(venti))
-			i = 1
+			i = 4
 		elif t > maxN and t is not None:
 			 GPIO.output(pinrele, GPIO.HIGH)
                          if t > maxN + deltav[1]:
@@ -82,28 +87,35 @@ while i < 1:
 			       GPIO.output(pinventi, GPIO.HIGH)
                          print('Temp={0:0.1f}*C Umidita={1:0.1f}%'.format(t, umi))
                          print('Temperatura diurna alta, stufa OFF, luci='+repr(luce)+', ventilatore='+repr(venti))
-               		 i = 1
+               		 i = 4
 		elif t >= minN and t <= maxN and t is not None:
                         GPIO.output(pinventi, GPIO.HIGH)
 			print ('Temp={0:0.1f}*C Umidita={1:0.1f}%'.format(t, umi))
                         print('Temperatura diurna OK, stufa OFF, luci='+repr(luce)+', ventilatore='+repr(venti))                       
-               		i = 1
+               		i = 4
 		elif t is None:
                         GPIO.output(pinventi, GPIO.HIGH)
                         GPIO.output(pinrele, GPIO.HIGH)
                         print('Il sensore la ha scialata, None')
 	                umi, t = Adafruit_DHT.read_retry(sensor, pinsensor, 5, 1)
-                elif t < 0:
+                        i = i + 1
+                        print i
+		elif t < 0 and t > 50:
                         GPIO.output(pinventi, GPIO.HIGH)
                         GPIO.output(pinrele, GPIO.HIGH)
                         print('Il sensore ha dei problemi esce dal range 0-50C*')
 	                umi, t = Adafruit_DHT.read_retry(sensor, pinsensor)
-                else:
+                        i = i + 1
+                        print i
+
+		else:
                         print('Il sensore la ha scialata')
 	                umi, t = Adafruit_DHT.read_retry(sensor, pinsensor, 5, 1)
+                        i = i + 1
+                        print i
 	else:
 		luce = GPIO.input(26)
-		print ('adafruit restituisce cazzate')
+		print ('localtime restituisce cazzate')
 
 
 #except KeyboardInterrupt:
--- VPDasp.py
+++ VPDasp.py
@@ -13,24 +13,29 @@ pinrele = 19
 f1 = range(19, 24)                      #
 f2 = range(0, 13)                       #
 luce = not(localtime()[3] in (f1 + f2)) #
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
 
 
-umi, t = Adafruit_DHT.read_retry(sensor, pinsensor)
+umi, t = Adafruit_DHT.read_retry(sensor, pinsensor, 1)
 i = 0
-while i < 1:
+while i < 4:
 	if t is not None:
 		ur = 100*(-0.9/(0.6108*math.exp(17.27*t/(t+237.3)))+1)
 		min = ur - 0.2
 		max = ur + 1.2
 		ming = ur - 0.4
 		maxg = ur + 1.4		
-		i = 1
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+		i = 4
+	else:
+		umi, t = Adafruit_DHT.read_retry(sensor, pinsensor, 1)
+		print ('la temperatura e None, riprovo altre '+repr(4-i-1)+' volte.')
+		i = i + 1
 
-print min 
-print max
-if luce == 1:
+#print min 
+#print max
+
+if luce == 1 and i == 4:
 	if umi > max and umi < 100 and umi is not None:
 				GPIO.setup(pinrele, GPIO.OUT)
 				GPIO.output(pinrele, GPIO.LOW)
@@ -52,7 +57,7 @@ if luce == 1:
 				GPIO.setup(pinrele, GPIO.OUT)
 				GPIO.output(pinrele, GPIO.HIGH)
 				print('Il sensore la ha scialata, aspirazione OFF')
-else:
+elif luce == 0 and i == 4:
         if umi > maxg and umi < 100 and umi is not None:
                                 GPIO.setup(pinrele, GPIO.OUT)
                                 GPIO.output(pinrele, GPIO.LOW)
