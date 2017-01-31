587beba01a7a2926cf24c0b8d18c020826b090dc
Tommaso Iaquinta <tommyiaq22@gmail.com>
2017-02-01 00:56:38 +0100

    Ho fatto delle modifiche nel calcolo di min e max, e ho dovuto cambiarli nome perche risultavano una funziona del modulo math

--- NOTTEGIORNOSTUFA.py
+++ NOTTEGIORNOSTUFA.py
@@ -3,6 +3,8 @@ from time import sleep
 import time
 import RPi.GPIO as GPIO
 from time import localtime
+import math
+
 sensor = Adafruit_DHT.DHT22
 pinsensor = 4
 sleepTime = 3
@@ -22,35 +24,56 @@ luce = not(localtime()[3] in (f1 + f2))	#
 
 #luce = GPIO.input(pinrele2)
 venti = GPIO.input(pinventi)
-min = 22
-max = 22.5
-deltavg = (1.3, 1.4)
+# amaro tentativo
+if t is not None and umi is not None:
+	VPDUR = 100*(-0.9/(0.6108*math.exp(17.27*t/(t+237.3)))+1)
+        VPDTEMP = (237.3*math.log((0.9)/(0.6108*(1-(umi/100))))/(17.27-math.log((0.9)/(0.6108*(1-(umi/100))))))
+        VPD = 6.108*math.exp(17.27*t/(t+237.3))*(1-(umi/100))
+	if VPD < 8  and umi > VPDUR:
+		minimo = VPDTEMP - 1
+		massimo = VPDTEMP - 0.5
+		deltavg = (1.0, 1.2)
+	elif VPD >= 8 and VPD < 8.2:
+		print
+	else:
+	        minimo = 22
+       		massimo = 22.4
+        	deltavg = (1.3, 1.5)
 
-minN = 19.4
-maxN = 19.7
-deltav = (1.1, 1.2)
+else:
+	minimo = 22
+        massimo = 22.4
+        deltavg = (1.3, 1.5)
+
+print minimo, massimo
+
+minN = 21
+maxN = 21.5
+deltav = (1.2, 1.3)
 
 i = 0
 
 while i < 4:
 	if luce == 0 and luce is not None:	 
-		if t < min and t is not None:
+		if t < minimo and t is not None:
 			GPIO.output(pinrele, GPIO.LOW)
-			GPIO.output(pinventi, GPIO.HIGH)
+			GPIO.output(pinventi, GPIO.LOW)
 			print('Temp={0:0.1f}*C Umidita={1:0.1f}%'.format(t, umi))
                         print('Temperatura diurna bassa, stufa ON, luci='+repr(luce)+', ventilatore='+repr(venti))
 			i = 4
-		elif t > max and t is not None:
+		elif t > massimo and t is not None:
 	                GPIO.output(pinrele, GPIO.HIGH)
-                        if t > max + deltavg[1]:
+                        if t > massimo + deltavg[1]:
 				GPIO.output(pinventi, GPIO.LOW)
-			elif t < max + deltavg[0]:
+			elif t > massimo + deltavg[0]:
+                                GPIO.output(pinventi, GPIO.LOW)
+			elif t <= massimo + deltavg[0]:
 	                        GPIO.output(pinventi, GPIO.HIGH)
 			print('Temp={0:0.1f}*C Umidita={1:0.1f}%'.format(t, umi))
                         print('Temperatura diurna alta, stufa OFF, luci='+repr(luce)+', ventilatore='+repr(venti))
 			i = 4
-		elif t >= min and t <= max and t is not None:
-                        GPIO.output(pinventi, GPIO.HIGH)
+		elif t >= minimo and t <= massimo and t is not None:
+#                        GPIO.output(pinventi, GPIO.LOW)
         	        print ('Temp={0:0.1f}*C Umidita={1:0.1f}%'.format(t, umi))
                         print('Temperatura diurna OK, stufa OFF, luci='+repr(luce)+', ventilatore='+repr(venti))
 			i = 4	
@@ -61,7 +84,7 @@ while i < 4:
 	                umi, t = Adafruit_DHT.read_retry(sensor, pinsensor, 5, 1)
                         i = i + 1
                         print i
-		elif t < 0 and t > 50:
+		elif t < 11 and t > 50:
                         GPIO.output(pinventi, GPIO.HIGH)
 			GPIO.output(pinrele, GPIO.HIGH)
 			print('Il sensore ha dei problemi esce dal range 0-50C*')
@@ -114,7 +137,7 @@ while i < 4:
                         i = i + 1
                         print i
 	else:
-		luce = GPIO.input(26)
+#		luce = GPIO.input(26)
 		print ('localtime restituisce cazzate')
 
 
